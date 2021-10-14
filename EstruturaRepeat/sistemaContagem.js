950c6e9b01580a0eabe09053894742e064605c51
Victor <victorlangnier2@gmail.com>
2021-10-14 18:46:46 -0300

    Sistema de contagem criado!

---
+++ EstruturaRepeat/sistemaContagem.js
@@ -0,0 +1,17 @@
+let totalAlunos = 10
+
+for (contador = 0; contador < 10; contador++)
+{
+    if (contador == 0)
+    {
+        console.log(contador + ": Zero")
+    }
+    else if (contador % 2)
+    {
+        console.log(contador + ": Ímpar")
+    }
+    else
+    {
+        console.log(contador + ": Par")
+    }
+}
